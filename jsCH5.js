9c29954884aad5908ca55f29848cc67037e393c4
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-04-02 22:54:49 +0700

    menambahkan video

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventLitster("click", function video(){
+    alert("imi video Cristiano");
+});
+
+Audio.addEventLitster("click", function audio(){
+    alert("ini audio SIUUUUU");
+});
+
+gambar.addEventLitster("click", function gambar(){
+    alert("ini gambar GOAT")
+})
